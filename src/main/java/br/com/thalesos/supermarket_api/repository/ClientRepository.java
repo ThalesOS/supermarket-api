fe571e1efe0b9e7fc3cd8870aec43280253ae9d7
Thales Oliveira <thales.oliveira2567@gmail.com>
2025-09-05 04:06:16 -0300

    feat: Create repository interfaces for all entities.

---
+++ src/main/java/br/com/thalesos/supermarket_api/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package br.com.thalesos.supermarket_api.repository;
+
+import br.com.thalesos.supermarket_api.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+}
